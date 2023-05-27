116a1015890717726be321923bd13975fc31ed3d
decoder <abdulbosit19980204@gmail.com>
2023-05-28 01:12:10 +0500

    Module operation system OS

--- 1-modul/9-dars-ModuleOperationSystemOS/modules/os.js
+++ 1-modul/9-dars-ModuleOperationSystemOS/modules/os.js
@@ -1,1 +1,19 @@
 const os = require('os')
+
+// platform
+
+console.log(os.platform());
+console.log(os.cpus());
+
+//architecture
+
+console.log(os.arch());
+
+//free memory
+console.log(os.freemem());
+
+//total memory
+console.log(os.totalmem());
+
+//entery point / home dir
+console.log(os.homedir());
