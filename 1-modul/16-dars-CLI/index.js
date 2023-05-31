896205e7315395bbd26c7ea38a499c5cd27c6052
decoder <abdulbosit19980204@gmail.com>
2023-06-01 03:57:10 +0500

    CLI

---
+++ 1-modul/16-dars-CLI/index.js
@@ -0,0 +1,48 @@
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const server = http.createServer((req, res) => {
+    console.log("method: ", req.method, " URL: ", req.url);
+    if (req.method === "GET") {
+        res.writeHead(200, { 'Content-type': 'text/html' })
+        if (req.url === '/') {
+            fs.readFile(path.join(__dirname, 'templates', 'index.html'), 'utf-8', (err, conten) => {
+                if (err) throw err
+                res.end(conten)
+            })
+
+        } else if (req.url === '/about') {
+            fs.readFile(path.join(__dirname, 'templates', 'about.html'), 'utf-8', (err, conten) => {
+                if (err) throw err
+                res.end(conten)
+            })
+
+        } else if (req.url === '/contact') {
+            fs.readFile(path.join(__dirname, 'templates', 'contact.html'), 'utf-8', (err, conten) => {
+                if (err) throw err
+                res.end(conten)
+            })
+
+        } else if (req.url === '/api/admin') {
+            res.writeHead(200, { 'Content-type': 'text/json' })
+            const admin = { name: "Abdulbosit", surname: "Tuychiev", job: "Fullstack developer" }
+            res.end(JSON.stringify(admin))
+        }
+    } else if (req.method === "POST") {
+        const name = []
+        res.writeHead(200, { 'Content-type': 'text/html; charset=utf-8' })
+
+        req.on('data', data => { name.push(Buffer.from(data)) })
+        req.on('end', () => {
+            const message = name.toString().split('=')[1]
+            res.end(`${message} registred successfully!`)
+        })
+    }
+
+})
+
+server.listen(3000, () => {
+
+    console.log("Server has been started on port: 3000");
+})
