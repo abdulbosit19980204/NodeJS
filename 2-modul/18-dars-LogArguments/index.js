b80ba8db6b0e40b698ad1c61759189ec252b9bc5
decoder <abdulbosit19980204@gmail.com>
2023-06-02 03:33:46 +0500

    Log Arguments

--- 2-modul/18-dars-LogArguments/index.js
+++ 2-modul/18-dars-LogArguments/index.js
@@ -1,5 +1,23 @@
+const getArgs = require('./helpers/args')
+
 const startCLI = () => {
-    console.log("start");
+    const args = getArgs(process.argv)
+
+    console.log(process.argv);
+
+    console.log(args);
+
+
+    if (args.h) {
+        //help
+    }
+    if (arga.a) {
+        //save city
+    }
+    if (args.t) {
+        // save token
+    }
+    //result
 }
 
 startCLI()
