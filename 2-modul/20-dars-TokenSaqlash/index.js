587da746b065eea7e7ca0e06ab71bbcf10d5eeb2
decoder <abdulbosit19980204@gmail.com>
2023-06-04 13:29:16 +0500

    Token Saqlash

--- 2-modul/20-dars-TokenSaqlash/index.js
+++ 2-modul/20-dars-TokenSaqlash/index.js
@@ -1,12 +1,19 @@
 import getArgs from './helpers/args.js'
 import { printError, printSuccess, printHelp } from './services/log.service.js'
-// const getArgs = require('./helpers/args')
-// const { printError, printSuccess } = require('./services/log.service')
+import { saveKeyValue } from './services/storage.service.js'
+
+const saveToken = async(token) => {
+    try {
+        await saveKeyValue('token', token)
+        printSuccess('Token was saved')
+    } catch (error) {
+        printError(error.message)
+    }
+}
+
 const startCLI = () => {
     const args = getArgs(process.argv)
-        // console.log("ARGS: ", args);
-        // printSuccess("Good job. Ok")
-        // printError("Print Error")
+    console.log("args:", args);
 
     if (args.h) {
         //help
@@ -17,6 +24,7 @@ const startCLI = () => {
     }
     if (args.t) {
         // save token
+        saveKeyValue('token', args.t)
     }
     //result
 }
