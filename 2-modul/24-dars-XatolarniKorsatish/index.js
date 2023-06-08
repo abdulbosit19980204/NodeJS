3a748388b53655eece932a67a77d4bc72175edd1
decoder <abdulbosit19980204@gmail.com>
2023-06-08 19:45:25 +0500

    Xatolarni korsatish

---
+++ 2-modul/24-dars-XatolarniKorsatish/index.js
@@ -0,0 +1,40 @@
+import getArgs from './helpers/args.js'
+import { getWeather } from './services/api.service.js'
+import { printError, printSuccess, printHelp } from './services/log.service.js'
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js'
+
+const saveToken = async(token) => {
+    if (!token.length) {
+        printError("Token doesn't exsist")
+        return
+    }
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.token, token)
+        printSuccess('Token was saved')
+    } catch (error) {
+        printError(error.message)
+    }
+}
+
+const startCLI = () => {
+    const args = getArgs(process.argv)
+    console.log("args:", args);
+    console.log("env:", process.env);
+
+    if (args.h) {
+        //help
+        printHelp()
+    }
+    if (args.a) {
+        //save city
+    }
+    if (args.t) {
+        // save token
+        return saveKeyValue(args.t)
+
+    }
+    getWeather(process.env.CITY ?? 'Uzbekistan')
+        //result
+}
+
+startCLI()
---
+++ 2-modul/24-dars-XatolarniKorsatish/services/api.service.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import { url } from 'inspector'
+import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js'
+
+const getWeather = async city => {
+    // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
+    const token = process.env.TOKEN ?? (await getKeyValue(TOKEN_DICTIONARY.token))
+    if (!token) {
+        throw new Error("API doesn't exist, -t [API_KEY] for saving token")
+    }
+
+    const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+        params: {
+            q: city,
+            appid: token,
+            lang: 'en',
+            units: 'metric',
+
+        }
+    })
+
+    console.log(data);
+    return data
+}
+
+export { getWeather }
