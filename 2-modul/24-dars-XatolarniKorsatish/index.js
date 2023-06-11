0f8d42faf3ab5b99aa034fb8fecc0a9e23360c71
decoder <abdulbosit19980204@gmail.com>
2023-06-11 23:58:38 +0500

    Xatoliklarni chiqarish

--- 2-modul/24-dars-XatolarniKorsatish/index.js
+++ 2-modul/24-dars-XatolarniKorsatish/index.js
@@ -18,12 +18,12 @@ const saveToken = async(token) => {
 
 const getForcast = async() => {
     try {
-        const response = await getWeather(process.env.CITY ? ? 'Uzbekistan')
+        const response = await getWeather(process.env.CITY ?? 'Uzbekistan')
         console.log(response);
     } catch (error) {
-        if (error ? .response ? .status == 404) {
+        if (error?.response?.status == 404) {
             printError("City not found")
-        } else if (error ? response ? .status == 401) {
+        } else if (error?.response?.status == 401) {
             printError("Invalid token")
         } else {
             printError(error.message)
