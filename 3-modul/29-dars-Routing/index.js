caa8eac52bba7f1444dcd0e2078ca74f5b700ea9
decoder <abdulbosit19980204@gmail.com>
2023-06-15 06:11:09 +0500

    Routing

--- 3-modul/29-dars-Routing/index.js
+++ 3-modul/29-dars-Routing/index.js
@@ -1,6 +1,25 @@
 import express from "express";
+import path, { dirname } from "path"
+import { fileURLToPath } from "url"
 
+
+const __filename = fileURLToPath(
+    import.meta.url)
+const __dirname = dirname(__filename)
 const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200)
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+        // res.send("Main page")
+
+})
+
+app.get('/about', (req, res) => {
+    res.status(200)
+    res.sendFile(path.join(__dirname, 'views', 'about.html'))
+        // res.send("About page")
+})
 
 const PORT = process.env.PORT || 4100
 app.listen(PORT, () => { console.log(`Server is running on port:  ${ PORT }`) })
