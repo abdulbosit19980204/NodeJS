e69c418b396bca52bde1240ac8b030ce6cf05b35
decoder <abdulbosit19980204@gmail.com>
2023-06-17 12:59:15 +0500

    Dynamic title

--- 3-modul/34-dars-DynamicTitle/routes/products.js
+++ 3-modul/34-dars-DynamicTitle/routes/products.js
@@ -3,14 +3,21 @@ import { Router } from "express";
 const router = Router()
 
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', { title: 'Boom shop | Course' })
 
 })
 router.get('/products', (req, res) => {
-    res.render('products')
+    res.render('products', {
+        title: 'Products | Boom Shop',
+        isProducts: true,
+
+    })
 })
 router.get('/add', (req, res) => {
-    res.render('add')
+    res.render('add', {
+        title: 'Add new item',
+        isAdd: true,
+    })
 })
 
 export default router
