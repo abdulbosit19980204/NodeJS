f7ba5942082d263c53d095ee69fa486e90537b92
decoder <abdulbosit19980204@gmail.com>
2023-06-18 00:43:22 +0500

    Login & Register

---
+++ 3-modul/35-dars-LoginRegister/routes/auth.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+
+const router = Router()
+router.get('/login', (req, res) => {
+    res.render('login', {
+        title: 'Login | Boom Shop',
+        isLogin: true,
+    })
+})
+
+router.get('/register', (req, res) => {
+    res.render('register', {
+        title: 'Register | Boom Shop',
+        isRegister: true,
+    })
+})
+
+
+export default router
