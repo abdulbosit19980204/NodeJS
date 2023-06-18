19a4ee9f92981214dc0ab80fd84e97ef490a9d6e
decoder <abdulbosit19980204@gmail.com>
2023-06-18 21:39:18 +0500

    Static Folder

--- 3-modul/36-dars-StaticFolder/index.js
+++ 3-modul/36-dars-StaticFolder/index.js
@@ -16,6 +16,7 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', './views')
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 app.use(AuthRoutes)
 app.use(ProductsRoutes)
