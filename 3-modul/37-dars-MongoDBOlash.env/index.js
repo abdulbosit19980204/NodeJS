aeb1090d62c293e47716f0b3ea394ab6d9976d2e
decoder <abdulbosit19980204@gmail.com>
2023-06-19 15:01:34 +0500

    Mongo DB connect

--- 3-modul/37-dars-MongoDBOlash.env/index.js
+++ 3-modul/37-dars-MongoDBOlash.env/index.js
@@ -1,8 +1,14 @@
-import express from "express";
+import express from "express"
 import { create } from "express-handlebars"
+import mongoose from "mongoose"
+
+//DOTENV
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+//ROUTER
 import AuthRoutes from "./routes/auth.js"
 import ProductsRoutes from "./routes/products.js"
-
 
 
 const app = express()
@@ -17,10 +23,16 @@ app.set('view engine', 'hbs')
 app.set('views', './views')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
+app.use(express.json())
 
 app.use(AuthRoutes)
 app.use(ProductsRoutes)
 
-
+// console.log(process.env.MONGO_URI);
 const PORT = process.env.PORT || 4100
+
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true
+})
 app.listen(PORT, () => { console.log(`Server is running on port:  ${ PORT }`) })
