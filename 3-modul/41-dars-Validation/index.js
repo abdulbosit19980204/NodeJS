2197354dc97f53380d638c58c8b49470582440d2
decoder <abdulbosit19980204@gmail.com>
2023-06-22 05:21:37 +0500

    Validation

---
+++ 3-modul/41-dars-Validation/index.js
@@ -0,0 +1,49 @@
+import express from "express"
+import { create } from "express-handlebars"
+import mongoose from "mongoose"
+
+//DOTENV
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+//ROUTER
+import AuthRoutes from "./routes/auth.js"
+import ProductsRoutes from "./routes/products.js"
+
+
+const app = express()
+
+const hbs = create({
+    defaultLayout: 'main',
+    extname: 'hbs',
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', './views')
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(express.json())
+
+app.use(AuthRoutes)
+app.use(ProductsRoutes)
+
+
+const startApp = () => {
+    try {
+        mongoose.set('strictQuery', false)
+        mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true
+        })
+        console.log("MongoDB connected");
+
+        // console.log(process.env.MONGO_URI);
+        const PORT = process.env.PORT || 4100
+        app.listen(PORT, () => { console.log(`Server is running on port:  ${ PORT }`) })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startApp()
---
+++ 3-modul/41-dars-Validation/routes/auth.js
@@ -0,0 +1,48 @@
+import { Router } from "express";
+import User from "../models/User.js";
+import bcrypt from "bcrypt"
+const router = Router()
+router.get('/login', (req, res) => {
+    res.render('login', {
+        title: 'Login | Boom Shop',
+        isLogin: true,
+    })
+})
+
+router.get('/register', (req, res) => {
+    res.render('register', {
+        title: 'Register | Boom Shop',
+        isRegister: true,
+    })
+})
+
+router.post('/login', async(req, res) => {
+    const existUser = await User.findOne({ email: req.body.email })
+    if (!existUser) {
+        console.log("User not found");
+        return false
+    }
+    const isPassEqual = await bcrypt.compare(req.body.password, existUser.password)
+    if (!isPassEqual) {
+        console.log("Password wrong");
+        return false
+    }
+    console.log(existUser);
+    res.redirect('/')
+})
+
+router.post('/register', async(req, res) => {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const userData = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: hashedPassword,
+    }
+
+    const user = await User.create(userData)
+    console.log(user);
+    res.redirect('/')
+})
+
+export default router
