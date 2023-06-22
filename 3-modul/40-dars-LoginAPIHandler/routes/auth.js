99af2492fa67ca5cfac355f341e5eab2526ed8c7
decoder <abdulbosit19980204@gmail.com>
2023-06-22 05:20:47 +0500

    Login API Handler

--- 3-modul/40-dars-LoginAPIHandler/routes/auth.js
+++ 3-modul/40-dars-LoginAPIHandler/routes/auth.js
@@ -16,8 +16,18 @@ router.get('/register', (req, res) => {
     })
 })
 
-router.post('/login', (req, res) => {
-    console.log(req.body);
+router.post('/login', async(req, res) => {
+    const existUser = await User.findOne({ email: req.body.email })
+    if (!existUser) {
+        console.log("User not found");
+        return false
+    }
+    const isPassEqual = await bcrypt.compare(req.body.password, existUser.password)
+    if (!isPassEqual) {
+        console.log("Password wrong");
+        return false
+    }
+    console.log(existUser);
     res.redirect('/')
 })
 
