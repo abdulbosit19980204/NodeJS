d9e452f895cf5cff7dfda7d180171c9f085f25de
decoder <abdulbosit19980204@gmail.com>
2023-06-28 15:57:49 +0500

    Dynamic navbar Logout btn

--- 3-modul/44-dars-DynamicNavbarCookie/routes/products.js
+++ 3-modul/44-dars-DynamicNavbarCookie/routes/products.js
@@ -3,7 +3,10 @@ import { Router } from "express";
 const router = Router()
 
 router.get('/', (req, res) => {
-    res.render('index', { title: 'Boom shop | Course' })
+    res.render('index', {
+        title: 'Boom shop | Course',
+        token: true,
+    })
 
 })
 router.get('/products', (req, res) => {
