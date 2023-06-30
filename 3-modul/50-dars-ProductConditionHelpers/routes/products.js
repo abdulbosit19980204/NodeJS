845106763000d1381ebd22a3d4c321a793f076f4
decoder <abdulbosit19980204@gmail.com>
2023-07-01 02:41:37 +0500

    Product Condition Helpers reverse

--- 3-modul/50-dars-ProductConditionHelpers/routes/products.js
+++ 3-modul/50-dars-ProductConditionHelpers/routes/products.js
@@ -11,7 +11,7 @@ router.get('/', async(req, res) => {
 
     res.render('index', {
         title: 'Boom shop | Course',
-        products: products
+        products: products.reverse()
     })
 
 })
