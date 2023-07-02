a8e8bfa1291dc7916ea03825121684817854db2e
decoder <abdulbosit19980204@gmail.com>
2023-07-02 08:10:17 +0500

    Product DEtail

--- 3-modul/53-dars-ProductDetail/routes/products.js
+++ 3-modul/53-dars-ProductDetail/routes/products.js
@@ -38,6 +38,17 @@ router.get('/add', authMiddleware, (req, res) => {
     })
 })
 
+
+router.get('/product/:id', async(req, res) => {
+    const id = req.params.id
+    const product = await Product.findById(id).populate("user").lean()
+    res.render('product', {
+        product: product,
+    })
+
+})
+
+
 router.post('/add-products', userMiddleware, async(req, res) => {
     const { title, description, image, price } = req.body
     if (!title || !description || !image || !price) {
